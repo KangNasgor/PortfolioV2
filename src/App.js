d1bf9f821787f7cbd815a4ee0c1ebfcac9d2a380
KangLeonard <leal643201@gmail.com>
2023-11-09 12:09:43 +0700

    Published

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import ReactDOM from "react-dom"
+import { BrowserRouter, Route, Routes, Link, useLocation} from "react-router-dom";
+import MainMenu from "./components/MainMenu/MainMenu";
+import Project from "./components/Project/Project";
+import About from "./components/About/About";
 
-function App() {
+export default function App() {
+  const textStyle = {
+    color: 'grey', 
+};
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+      <ul className='navbar'>
+      <li>
+        <Link to="/"><a>Home</a></Link>
+      </li>
+      <li>
+        <Link to="/project"><a>Projects</a></Link>
+      </li>
+      <li>
+        <Link to="/about"><a>About</a></Link>
+      </li>
+      </ul>
+        <Routes>
+          <Route path="/" element={<MainMenu />} />
+          <Route path="/project" element={<Project />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
 
-export default App;
