64059becbe233eeb576550e164d11e57278b4bcb
KangLeonard <leal643201@gmail.com>
2023-11-09 12:51:15 +0700

    I think this fixes blank page

--- src/index.js
+++ src/index.js
@@ -2,8 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter, BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 const app = ReactDOM.createRoot(document.getElementById('mainmenu'));
 app.render(<App />);
-
